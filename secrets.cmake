56d56d9a7080cb8350e1904787b103eda2b79dad
Weng Jin Rui <131884797+AolaAolaAola@users.noreply.github.com>
2025-11-23 23:40:19 +0800

    Add files via upload

---
+++ secrets.cmake
@@ -0,0 +1,6 @@
+# secrets.cmake
+# This file defines sensitive credentials and should be excluded from version control.
+# Pico W only accepts 2.4 GHz bandwidth
+set(WIFI_SSID "REPLACE WITH WIFI SSID")
+set(WIFI_PASSWORD "REPLACE WITH WIFI PASSWORD")
+
